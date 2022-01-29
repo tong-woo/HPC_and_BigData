7e2e4bf2c15cced0abe618ee2e6a306d89520270
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2022-01-29 18:47:33 +0100

    [MPI]

--- 4.Parallel_Programming_with_MPI/mat_vec_mul.c
+++ 4.Parallel_Programming_with_MPI/mat_vec_mul.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     double* MATRIX;
     double* VECTOR_V;
     double* VECTOR_RESULT;
-    int DIMENSION_SIZE = 10000; // N
+    int DIMENSION_SIZE = 100000; // N
 
     Allocate_dynamic_arrays(&MATRIX, &VECTOR_V, &VECTOR_RESULT, DIMENSION_SIZE);
 
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
             VECTOR_V,
             MATRIX
         );
-        Print_vector("R", VECTOR_R, DIMENSION_SIZE);
+        //Print_vector("R", VECTOR_R, DIMENSION_SIZE);
         const double finish = MPI_Wtime();
         printf("Stopped as master. This took %.4f seconds\n", finish-start);
     } else {
