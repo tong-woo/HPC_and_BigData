1e5cdb23818cb291d5d8d194cd0782775263882f
Tong Wu <jimwu@kaist.ac.kr>
2022-01-31 00:30:42 +0100

    openmp

--- 4.Parallel_Programming_with_MPI/name_sbatch.sh
+++ 4.Parallel_Programming_with_MPI/name_sbatch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 #SBATCH -t 1:10 
-#SBATCH -N 2 --ntasks-per-node=1
+#SBATCH -N 2 --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=normal
+
+export OMP_NUM_THREADS=16
 
 mpirun ./mat_vec_mul
