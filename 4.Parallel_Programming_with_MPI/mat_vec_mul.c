add46361dc9729283b6433548df38b4f6aca5669
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2022-01-30 18:43:55 +0100

    [MPI 3]

--- 4.Parallel_Programming_with_MPI/mat_vec_mul.c
+++ 4.Parallel_Programming_with_MPI/mat_vec_mul.c
@@ -89,16 +89,16 @@ int main(int argc, char *argv[]) {
         bool is_last_iteration = false;
         for(int i=1; i<=R; i++) {
                 if(i==R) is_last_iteration = true;
-                    double * VECTOR_R = run_as_master(
-                        workers, 
-                        DIMENSION_SIZE,
-                        VECTOR_RESULT,
-                        VECTOR_V,
-                        MATRIX,
-                        is_last_iteration
-                    );
+                double * VECTOR_R = run_as_master(
+                    workers, 
+                    DIMENSION_SIZE,
+                    VECTOR_RESULT,
+                    VECTOR_V,
+                    MATRIX,
+                    is_last_iteration
+                );
+                Print_vector("R", VECTOR_R, DIMENSION_SIZE);
         }
-        Print_vector("R", VECTOR_R, DIMENSION_SIZE);
         const double finish = MPI_Wtime();
         printf("Stopped as master. This took %.4f seconds\n", finish-start);
     } else {
