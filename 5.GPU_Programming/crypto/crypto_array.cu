718b42a768e8de05478a60f83c131b93794c46a0
Zhaolin   Fang <lcur0229@test3.irc.sara.nl.irc.sara.nl>
2022-02-07 20:06:59 +0100

    finish test

--- 5.GPU_Programming/crypto/crypto_array.cu
+++ 5.GPU_Programming/crypto/crypto_array.cu
@@ -30,7 +30,19 @@ __global__ void encryptKernel(int len, char* deviceDataIn, char* deviceDataOut) 
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
     //int key_len = sizeof(key) / sizeof(int) + 1;
 
-    int key[] ={1, 2, 3};
+    int key[] ={1,2,3,4,5,6,7,8,9,10,
+		11,12,13,14,15,16,17,18,19,20,
+		21,22,23,24,25,26,27,28,29,30,
+		31,32,33,34,35,36,37,38,39,40,
+		41,42,43,44,45,46,47,48,49,50,
+		51,52,53,54,55,56,57,58,59,60,
+		61,62,63,64,65,66,67,68,69,70,
+		71,72,73,74,75,76,77,78,79,80,
+		81,82,83,84,85,86,87,88,89,90,
+		91,92,93,94,95,96,97,98,99,100,
+		101,102,103,104,105,106,107,108,109,110,
+		111,112,113,114,115,116,117,118,119,120,
+		121,122,123,124,125,126,127,128};
     int key_cur = index % len;
 
     if (deviceDataIn[index] >= 'a' && deviceDataIn[index] <= 'z')
@@ -46,11 +58,23 @@ __global__ void decryptKernel(int len, char* deviceDataIn, char* deviceDataOut) 
     //int key_len = sizeof(key) / sizeof(int) + 1;
     int key_cur = index % len;
     
-    int key[] ={1, 2, 3};
+    int key[] ={1,2,3,4,5,6,7,8,9,10,
+		11,12,13,14,15,16,17,18,19,20,
+		21,22,23,24,25,26,27,28,29,30,
+		31,32,33,34,35,36,37,38,39,40,
+		41,42,43,44,45,46,47,48,49,50,
+		51,52,53,54,55,56,57,58,59,60,
+		61,62,63,64,65,66,67,68,69,70,
+		71,72,73,74,75,76,77,78,79,80,
+		81,82,83,84,85,86,87,88,89,90,
+		91,92,93,94,95,96,97,98,99,100,
+		101,102,103,104,105,106,107,108,109,110,
+		111,112,113,114,115,116,117,118,119,120,
+		121,122,123,124,125,126,127,128};
     if (deviceDataIn[index] >= 'a' && deviceDataIn[index] <= 'z')
-	deviceDataOut[index] = (deviceDataIn[index] - 'a' - key[key_cur] + 26) % 26 + 97;
+	deviceDataOut[index] = (deviceDataIn[index] - 'a' - (key[key_cur]%26) + 26) % 26 + 97;
     else if (deviceDataIn[index] >= 'A' && deviceDataIn[index] <= 'Z')
-	deviceDataOut[index] = (deviceDataIn[index] - 'A' - key[key_cur] + 26) % 26 + 65;
+	deviceDataOut[index] = (deviceDataIn[index] - 'A' - (key[key_cur]%26) + 26) % 26 + 65;
     else
 	deviceDataOut[index] = deviceDataIn[index];
 }
@@ -173,9 +197,9 @@ int DecryptSeq (int n, int key[], int len, char* data_in, char* data_out)
   {
     key_cur = i % len; 
     if (data_in[i] >= 'a' && data_in[i] <= 'z')
-	data_out[i] = (data_in[i] - 'a' - key[key_cur] + 26 ) % 26 + 97;
+	data_out[i] = (data_in[i] - 'a' - (key[key_cur]%26) + 26 ) % 26 + 97;
     else if (data_in[i] >= 'A' && data_in[i] <= 'Z')
-	data_out[i] = (data_in[i] - 'A' - key[key_cur] + 26) % 26 + 65;
+	data_out[i] = (data_in[i] - 'A' - (key[key_cur]%26) + 26) % 26 + 65;
     else
         data_out[i] = data_in[i];
   }
@@ -207,8 +231,7 @@ int EncryptCuda (int n, int len, char* data_in, char* data_out) {
         cout << "could not allocate memory!" << endl;
         return -1;
     }
-    
-
+   
     timer kernelTime1 = timer("kernelTime");
     timer memoryTime = timer("memoryTime");
 
@@ -294,8 +317,21 @@ int DecryptCuda (int n, int len, char* data_in, char* data_out) {
 
 int main(int argc, char* argv[]) {
     int n;
-    int key[] ={1, 2, 3};
+    int key[] ={1,2,3,4,5,6,7,8,9,10,
+		11,12,13,14,15,16,17,18,19,20,
+		21,22,23,24,25,26,27,28,29,30,
+		31,32,33,34,35,36,37,38,39,40,
+		41,42,43,44,45,46,47,48,49,50,
+		51,52,53,54,55,56,57,58,59,60,
+		61,62,63,64,65,66,67,68,69,70,
+		71,72,73,74,75,76,77,78,79,80,
+		81,82,83,84,85,86,87,88,89,90,
+		91,92,93,94,95,96,97,98,99,100,
+		101,102,103,104,105,106,107,108,109,110,
+		111,112,113,114,115,116,117,118,119,120,
+		121,122,123,124,125,126,127,128};
     int key_len = sizeof(key) / sizeof(int);
+    printf("The length of key:%d\n",key_len);
     n = fileSize();
 
     if (n == -1) {
